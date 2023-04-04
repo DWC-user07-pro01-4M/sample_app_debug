61aa966d0305a7b83238ed857d259fc134ded81d
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-04 08:28:51 +0000

    [Fix]ルーティング、application.html.erbの編集、lists_controller.rbの記述修正

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,42 +4,37 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  def index
-    @lists = List.all
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
-  def edit
-    @list = List.find(params[:id])
+  def index
+    @lists = List.all
   end
 
   def show
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
 
   def update
-    @list = List.find(params[:id])
-    @list.update(list_params)
-    redirect_to list_path(@list)
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
-
-
-
-  def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to list_path(@list.id)
-    else
-      render :index
-    end
-  end
-
-
 
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to list_path(list)  # 投稿一覧画面へリダイレクト
+    redirect_to lists_path  # 投稿一覧画面へリダイレクト
   end
 
   private
